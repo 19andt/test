083f02e3c3bc394516716b3fd71dda10ca2f3863
19andt <19andt@gmail.com>
2018-08-08 19:24:33 +0530

    Adding extra minifid files.

---
+++ static/js/min/update-interests.module.js
@@ -0,0 +1,1 @@
+"use strict";var app=angular.module("updateInterests",[]);app.service("updateInterestsService",function($http,$resource){var url="/interest/update_interests";return $resource(url,{},{post:{method:"POST",params:{},isArray:!1,cache:!1}})})
